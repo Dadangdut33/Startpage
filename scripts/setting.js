b7dea791d55e1e0e4e7ae5f9a9526a53eb5818e2
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-10-18 22:02:37 +0700

    cover image setting

--- scripts/setting.js
+++ scripts/setting.js
@@ -2,19 +2,36 @@ const titleConstant = "winTitle";
 const usernameConstant = "user";
 const showSecondsConstant = "showSeconds";
 const amPmConstant = "amPm";
+const selectCoverImgConstant = "selectCoverImage";
+const coverImgConstant = "coverImg";
 
 const username_Display = document.getElementById("user-display");
+const coverImg_Display = document.getElementById("coverImg-display");
 
 const winTitle_Input = document.getElementById(titleConstant);
 const username_Input = document.getElementById(usernameConstant);
 const showSeconds_Input = document.getElementById(showSecondsConstant);
 const amPm_Input = document.getElementById(amPmConstant);
+const selectCoverImg_Input = document.getElementById(selectCoverImgConstant);
+const coverImg_Input = document.getElementById(coverImgConstant);
+
+// ------------------------------
+const coverImageMap = {
+	pic_1: "images/clouds.gif",
+	pic_2: "images/lein.gif",
+	pic_3: "images/violet.gif",
+	pic_4: "images/white.gif",
+	pic_5: "images/blue.gif",
+	pic_6: "images/69414913.gif",
+};
 
 function init_Setting() {
 	const winTitle = localStorage.getItem(titleConstant);
 	const username = localStorage.getItem(usernameConstant);
 	const showSeconds = localStorage.getItem(showSecondsConstant);
 	const amPm = localStorage.getItem(amPmConstant);
+	const selectCoverImg = localStorage.getItem(selectCoverImgConstant);
+	const coverImg = localStorage.getItem(coverImgConstant);
 
 	if (winTitle) {
 		winTitle_Input.value = winTitle;
@@ -45,6 +62,22 @@ function init_Setting() {
 		amPm_Input.checked = false;
 		localStorage.setItem(amPmConstant, false);
 	}
+
+	if (selectCoverImg) {
+		selectCoverImg_Input.value = selectCoverImg;
+	} else {
+		selectCoverImg_Input.value = "pic_1";
+		localStorage.setItem(selectCoverImgConstant, "pic_1");
+	}
+
+	if (coverImg) {
+		coverImg_Input.value = coverImg;
+		coverImg_Display.src = coverImg;
+	} else {
+		coverImg_Input.value = coverImageMap["pic_1"];
+		coverImg_Display.src = coverImageMap["pic_1"];
+		localStorage.setItem(coverImgConstant, coverImageMap["pic_1"]);
+	}
 }
 
 init_Setting();
@@ -65,4 +98,20 @@ showSeconds_Input.onclick = (e) => {
 
 amPm_Input.onclick = (e) => {
 	localStorage.setItem(amPmConstant, amPm_Input.checked);
+};
+
+selectCoverImg_Input.onchange = (e) => {
+	if (selectCoverImg_Input.value == "custom") {
+		localStorage.setItem(selectCoverImgConstant, "custom");
+		coverImg_Input.disabled = false;
+	} else {
+		localStorage.setItem(selectCoverImgConstant, selectCoverImg_Input.value);
+		coverImg_Input.value = coverImageMap[selectCoverImg_Input.value];
+		coverImg_Display.src = coverImageMap[selectCoverImg_Input.value];
+	}
+};
+
+coverImg_Input.onkeyup = (e) => {
+	localStorage.setItem(coverImgConstant, coverImg_Input.value);
+	coverImg_Display.src = coverImg_Input.value;
 };
